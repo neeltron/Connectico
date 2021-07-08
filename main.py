f7b41e0cccace703406f04e99e4d5b67f4e05597
Neel Adwani <nprosofficial@gmail.com>
2021-07-08 04:32:11 +0000

    installed simple websocket

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from flask import Flask
-app = Flask('app')
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(
+  __name__,
+  template_folder='templates',
+  static_folder='static'
+)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
-app.run(host='0.0.0.0', port=8080)
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+if __name__ == '__main__':
+    socketio.run(app, host = '0.0.0.0', debug=True, port = 8080)
+    
