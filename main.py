57fa324557ea6df219c3fde743e815ab94729e26
Neel Adwani <nprosofficial@gmail.com>
2021-07-08 09:38:07 +0000

    Fixed: Socket Version Mismatch

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = Flask(
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
